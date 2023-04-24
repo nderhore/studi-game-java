7b46dd12170ee4e00a16dea7ace49b54775c7bd5
Nathan Derhore <nathan@derhore.fr>
2023-04-24 18:59:24 +0200

    implementation deviner le nombre

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,39 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //instanciation de l'objet Random
+        Random random = new Random();
+
+        //Generation du nombre aléatoire
+        int nombreADeviner = random.nextInt(100) +1;
+
+        //Instanciation de l'objet Scanner
+        Scanner sc = new Scanner(System.in);
+
+        int saisie = 0;
+
+        int cptEssai = 5;
+
+        System.out.println("Devinez un nombre entre 1 et 100.");
+
+        do {
+            System.out.println("Entrez un nombre : ");
+            saisie = sc.nextInt();
+            cptEssai--;
+
+            if(nombreADeviner < saisie){
+                System.out.println("le nombre a deviner est plus petit");
+            } else if(nombreADeviner > saisie){
+                System.out.println("le nombre a deviner est plus grand");
+            } else{
+                System.out.println("Bravo, vous avez deviné le nombre");
+            }
+
+        }while(saisie != nombreADeviner && cptEssai != 0);
+
+        if(saisie != nombreADeviner && cptEssai == 0){
+            System.out.println("Vous avez perdu");
+        }
+
     }
 }
